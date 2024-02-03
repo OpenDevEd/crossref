c7e0f258ac6ae826f57e41767513af1820b91d21
bjohas <bjohas+git@gmail.com>
2024-02-03 16:03:14 +0000

    Added saving to file and usage info

--- crossref.js
+++ crossref.js
@@ -1,17 +1,29 @@
 const axios = require('axios');
-
+const fs = require('fs');
 let dois = process.argv.slice(2);
 
-//console.log(dois)
+if (dois.length === 0) {
+  console.log('Usage: node crossref.js <doi> [<doi>...]');
+} else {
+  works(dois);
+};
 
-// Make a post request to the API https://api.openalex.org/works with parameters per-page=1 and search=XXX
-axios
-  .get('https://api.crossref.org/works/' + dois[0])
-  .then(function (response) {
-    // handle success
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
+function works(dois) {
+  // iterate through the array of DOIs
+  dois.forEach(function (doi) {
+    axios
+      .get('https://api.crossref.org/works/' + doi)
+      .then(function (response) {
+        // handle success
+        // url encode doi
+        const file = encodeURIComponent(doi) + ".json";
+        fs.writeFileSync(file,
+          JSON.stringify(response.data.message));
+        console.log(doi + ": success; saved to " + file);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(doi + ": " + error);
+      });
   });
+};
