38a01596437d5cd4be8daa59497d2b99ebe825e9
Hassan Mansour <hasana13work@gmail.com>
2024-02-03 14:52:11 +0100

    changed request to axios

--- crossref.js
+++ crossref.js
@@ -1,14 +1,17 @@
-const request = require('request');
+const axios = require('axios');
 
 let dois = process.argv.slice(2);
 
 //console.log(dois)
 
 // Make a post request to the API https://api.openalex.org/works with parameters per-page=1 and search=XXX
-request({
-  url: 'https://api.crossref.org/works/'+dois[0]
-}, function (error, response, body) {
-  if (!error && response.statusCode === 200) {
-    console.log(JSON.stringify(body))
-  }
-})
+axios
+  .get('https://api.crossref.org/works/' + dois[0])
+  .then(function (response) {
+    // handle success
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  });
